112e64cadeef879e275b7f18c183054e59fb7482
Rafael Reali <rafaelreali2011@hotmail.com>
2025-10-13 21:55:45 -0300

    fix:checkout page 3

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,15 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.SUPABASE_URL
-const SUPABASE_ANON_KEY = import.meta.env.SUPABASE_ANON_KEY
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-function cleanSupabaseUrl(url) {
-  if (typeof url === 'string' && url.endsWith('/')) {
-    return url.slice(0, -1);
-  }
-  return url;
-}
-
-const cleanedUrl = cleanSupabaseUrl(SUPABASE_URL);
-
-export const supabase = createClient(cleanedUrl, SUPABASE_ANON_KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
